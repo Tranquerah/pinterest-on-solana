efb95eeb6f67950341beb402008bfe8878e05216
Tengkera <67328864+Tranquerah@users.noreply.github.com>
2021-11-07 09:15:49 +0100

    Add phantom wallet

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
+/*
+ * We are going to be using the useEffect hook!
+ */
+import { useEffect } from 'react';
+
 // Constants
 const TWITTER_HANDLE = 'nobody';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  /*
+   * This function holds the logic for deciding if a Phantom Wallet is
+   * connected or not
+   */
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet found!');
+        }
+      } else {
+        alert('Solana object not found! Get a Phantom Wallet 👻');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  /*
+   * When our component first mounts, let's check to see if we have a connected
+   * Phantom Wallet
+   */
+  useEffect(() => {
+    window.addEventListener('load', async (event) => {
+      await checkIfWalletIsConnected();
+    });
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🖼 Pinterest on Solana</p>
+          <p className="header">🖼 GIF Portal</p>
           <p className="sub-text">
-            View your media collection on Solana ✨
+            View your GIF collection in the metaverse ✨
           </p>
         </div>
         <div className="footer-container">
